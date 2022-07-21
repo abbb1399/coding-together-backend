ec05dc0fe27045f47fd3e3fa09cfdf94d72f10cb
abbb1399 <abbb123@naver.com>
2022-07-21 23:53:52 +0900

    칸반 타이틀 변경 백엔드

--- src/routers/kanban.js
+++ src/routers/kanban.js
@@ -1,121 +1,137 @@
-const express = require('express')
-const Kanban = require('../models/kanban')
+const express = require("express")
+const Kanban = require("../models/kanban")
 const router = new express.Router()
-const auth = require('../middleware/auth')
+const auth = require("../middleware/auth")
 
 // Kanban Board 생성
-router.post('/kanbans', auth, async (req,res) => {
+router.post("/kanbans", auth, async (req, res) => {
   const kanban = new Kanban({
     ...req.body,
-    owner: req.user._id
+    owner: req.user._id,
   })
 
-  try{
+  try {
     await kanban.save()
     res.status(201).send(kanban)
-  }catch(e){
+  } catch (e) {
     res.status(400).send(e)
   }
 })
 
 // Kanban Board 불러오기
-router.get('/kanbans', auth, async (req,res) => {
-  try{
-    const kanbans = await Kanban.find({owner: req.user._id})
+router.get("/kanbans", auth, async (req, res) => {
+  try {
+    const kanbans = await Kanban.find({ owner: req.user._id })
     res.send(kanbans)
-  }catch(e){
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+// Kanban Board 이름 변경
+router.patch("/kanbans", auth, async (req, res) => {
+  const { boardId, title } = req.body
+
+  try {
+    const kanban = await Kanban.findOne({ _id: boardId })
+
+    if (!kanban) {
+      return res.status(404).send()
+    }
+
+    kanban.title = title
+    
+    await kanban.save()
+
+    res.send()
+  } catch (e) {
     res.status(500).send()
   }
 })
 
 // Task 생성
-router.patch('/tasks', async (req,res) => {
-  const {boardId, id, name} = req.body
-  
-  try{
-    const kanban = await Kanban.findOne({ _id : boardId})
-    
-    if(!kanban){
+router.patch("/tasks", async (req, res) => {
+  const { boardId, id, name } = req.body
+
+  try {
+    const kanban = await Kanban.findOne({ _id: boardId })
+
+    if (!kanban) {
       return res.status(404).send()
     }
-  
-    kanban.list.push({id, name})
+
+    kanban.list.push({ id, name })
     await kanban.save()
 
     res.send()
-  }catch(e){
+  } catch (e) {
     res.status(500).send()
   }
 })
 
 // Task 이동
-router.patch('/move-task', async(req,res) => {
-  const {status, boardId, task} = req.body
+router.patch("/move-task", async (req, res) => {
+  const { status, boardId, task } = req.body
 
-  try{
-    if(status ==='added'){
+  try {
+    if (status === "added") {
       await Kanban.findOneAndUpdate(
-        {_id: boardId},
-        {$push: {list:{id:task.id, name: task.name }}}
-      ) 
-    }else if(status === 'removed'){
+        { _id: boardId },
+        { $push: { list: { id: task.id, name: task.name } } }
+      )
+    } else if (status === "removed") {
       await Kanban.findOneAndUpdate(
-        {_id: boardId},
-        {$pull: {list:{id:task.id}}}
+        { _id: boardId },
+        { $pull: { list: { id: task.id } } }
       )
     }
     res.send()
-  }catch(e){
+  } catch (e) {
     res.status(500).send()
   }
 })
 
 // Task 이름 변경
-router.patch('/update-task', async (req,res) => {
-  const {status, boardId, taskId, taskName, taskDate} = req.body
+router.patch("/update-task", async (req, res) => {
+  const { status, boardId, taskId, taskName, taskDate } = req.body
 
-  let searchConditon 
+  let searchConditon
 
-  if(status === 'NAME'){
-    searchConditon = {$set: {"list.$[el].name": taskName } }
-  }else if(status === 'DATE'){
-    searchConditon =  {$set: {"list.$[el].date": taskDate } }
+  if (status === "NAME") {
+    searchConditon = { $set: { "list.$[el].name": taskName } }
+  } else if (status === "DATE") {
+    searchConditon = { $set: { "list.$[el].date": taskDate } }
   }
 
-  try{
-    await Kanban.findOneAndUpdate(
-      {_id: boardId},
-      searchConditon,
-      { 
-        arrayFilters: [{ "el.id": taskId }],
-        new: true
-      }
-    ) 
-      
+  try {
+    await Kanban.findOneAndUpdate({ _id: boardId }, searchConditon, {
+      arrayFilters: [{ "el.id": taskId }],
+      new: true,
+    })
+
     res.send()
-  }catch(e){
+  } catch (e) {
     res.status(500).send()
   }
 })
 
-
 // Task 순서 변경
-router.patch('/change-task-order', async (req,res) => {
-  const {boardId, element, newIndex, oldIndex} = req.body
-  try{
-    const kanban = await Kanban.findOne({ _id : boardId})
-  
-    if(!kanban){
+router.patch("/change-task-order", async (req, res) => {
+  const { boardId, element, newIndex, oldIndex } = req.body
+  try {
+    const kanban = await Kanban.findOne({ _id: boardId })
+
+    if (!kanban) {
       return res.status(404).send()
     }
 
     kanban.list.splice(oldIndex, 1)
-    
-    kanban.list.splice(newIndex, 0, element);
-    
+
+    kanban.list.splice(newIndex, 0, element)
+
     await kanban.save()
     res.send()
-  }catch(e){
+  } catch (e) {
     res.status(500).send()
   }
 })
